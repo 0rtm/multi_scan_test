c3f7441813391067e3ef368cc35bbc44c657bb43
Artem Tselikov <ar.tselikov@gmail.com>
2020-06-16 13:15:08 -0400

    initial setup

---
+++ DemoApp/DemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoApp
+//
+//  Created by Artem Tselikov on 2020-06-16.
+//  Copyright © 2020 Artem Tselikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
